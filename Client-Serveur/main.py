9a244cad7ab3c8efa5ea195b537a1b84dccf1c40
Jean-Christophe MARTIN <jeanchristophemartin373@gmail.com>
2023-11-06 16:57:53 +0100

    Commit

--- Client-Serveur/main.py
+++ Client-Serveur/main.py
@@ -1,2 +1,12 @@
+import socket
 
 if __name__ == '__main__':
+    host ="10.128.6.12"
+    port = 4090
+    message = input("votre message : ")
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+    client_socket.send(message.encode())
+    reply ="ok"
+    client_socket.recv(1024).decode()
+    client_socket.close()
