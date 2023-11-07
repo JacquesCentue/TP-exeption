4bc2189a7c925b96866dc007840efaabff99c284
Jean-Christophe MARTIN <44636654+JacquesCentue@users.noreply.github.com>
2023-11-07 17:11:12 +0100

    Add files via upload

---
+++ Client-Serveur/client.py
@@ -0,0 +1,21 @@
+
+import socket
+
+
+server = socket.socket()
+
+
+port = 4090
+addr_ip = "10.128.1.65"
+server.connect((socket.gethostbyaddr(addr_ip)[0], port))
+
+
+while True:
+    print(server.recv(1024))
+    server.send("Bonjour ici le client")
+    if msgClient.upper()== "FIN" or msgClient == "":
+       break
+    msgServeur = raw_input("S> ")
+    c.send(msgServeur)
+    # fermeture du socket
+    server.close()
---
+++ Client-Serveur/server.py
@@ -0,0 +1,39 @@
+
+# -*- coding: latin-1 -*-
+
+import socket # Import socket module
+
+s = socket.socket()
+nom_hote = socket.gethostname()
+addr_ip_hote = socket.gethostbyname(nom_hote)
+
+
+port = 4090
+s.bind((nom_hote, port))
+
+print("Serveur de test d'envoi de messages")
+print ("->nom du serveur %s"%nom_hote)
+print( "->IP de l'hote : %s"%addr_ip_hote)
+print( "->port :%i"%port)
+print("Serveur démarré, en attente d'une connexion ...")
+
+
+s.listen(5)
+
+
+c, addr = s.accept()
+addr_ip_client = addr[0]
+print("Connection recue depuis le client : ",addr_ip_client)
+
+message="Connecté au serveur !"
+c.send(message.encode())
+
+while True:
+    msgClient = c.recv(1024)
+    print ("C>", msgClient)
+    if msgClient.upper()== "FIN" or msgClient == "":
+        break
+        msgServeur = raw_input("S> ")
+        c.send(msgServeur)
+# on ferme la connexion
+        c.close()
