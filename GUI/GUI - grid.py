d109060a139eb09109609260c7fc8be7b98171ce
Jean-Christophe Martin <jean-christophe.martin@uha.fr>
2023-11-14 17:08:29 +0100

    GUI master commit

--- GUI/GUI - grid.py
+++ GUI/GUI - grid.py
@@ -1,4 +1,6 @@
 import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class MainWindows(QWidget):
@@ -7,24 +9,41 @@ class MainWindows(QWidget):
         self.setWindowTitle("une premiere fenetre")
         self.setLayout(QGridLayout())
         self.resize(250,100)
-        my_label = QLabel("Saisir votre nom :")
-        my_entry=QLineEdit()
-        my_entry.setObjectName("nom")
-        my_entry.setText("")
+        lblTemp = QLabel("Température")
+
+        tbxTemperature=QLineEdit()
+        self.QlineEdit = QLineEdit(tbxTemperature)
+        self.QlineEdit.setValidator(QIntValidator())
+        self.QlineEdit.setObjectName("temperature")
+        self.QlineEdit.setText("")
+
+
+
         my_label2 = QLabel("")
         my_button = QPushButton("OK",clicked =lambda: ok())
-        my_button2= QPushButton("Quitter",clicked =lambda: quit())
 
-        self.layout().addWidget(my_label,0, 0)
-        self.layout().addWidget(my_entry,0, 1)
+        cbxconversionType=QComboBox()
+        cbxconversionType.addItem("°C -> °K")
+        cbxconversionType.addItem("°K -> °C")
+
+
+        my_button2 = QPushButton("Quitter",clicked =lambda: quit())
+
+        self.layout().addWidget(lblTemp,0, 0)
+        self.layout().addWidget(self.QlineEdit,0, 1)
         self.layout().addWidget(my_label2,1, 0)
         self.layout().addWidget(my_button,1, 1)
+        self.layout().addWidget(cbxconversionType,1,2)
         self.layout().addWidget(my_button2,2, 2)
 
         def ok():
-            if my_entry.text() !="":
-                my_label2.setText(f'Bonjour {my_entry.text()}')
-                my_entry.setText("")
+
+            
+                if self.QlineEdit.text() > -273 :
+                    my_label2.setText(f'Bonjour {self.QlineEdit.text().text()}')
+
+
+
 
 
 
