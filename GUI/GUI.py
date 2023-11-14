3fed0c7103040be7360c92f46b5a9cbe48c07490
Jean-Christophe Martin <jean-christophe.martin@uha.fr>
2023-11-14 16:11:21 +0100

    GUI commit

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,13 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget,QVBoxLayout
-from PyQt5 import *
-app = QApplication(sys.argv)
-root = QWidget()
-root.resize(250, 250)
-root.setWindowTitle("test")
-root.setLayout(QVBoxLayout())
-QVBoxLayout.addWidget(PyQt5.Qlabel)
+from PyQt5.QtWidgets import *
 
-root.show()
+class MainWindows(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("une premiere fenetre")
+        self.setLayout(QVBoxLayout())
+        my_label = QLabel("Saisir votre nom :")
+        my_entry=QLineEdit()
+        my_entry.setObjectName("nom")
+        my_entry.setText("")
+        my_label2 = QLabel("")
+        my_button = QPushButton("OK",clicked =lambda: ok())
+        my_button2= QPushButton("Quitter",clicked =lambda: quit())
+
+        self.layout().addWidget(my_label)
+        self.layout().addWidget(my_entry)
+        self.layout().addWidget(my_label2)
+        self.layout().addWidget(my_button)
+        self.layout().addWidget(my_button2)
+
+        def ok():
+            if my_entry.text() !="":
+                my_label2.setText(f'Bonjour {my_entry.text()}')
+                my_entry.setText("")
+
+
+
+        self.show()
+
+
 if __name__ == '__main__':
-    sys.exit(app.exec_())
+    app=QApplication([])
+    mw=MainWindows()
+    app.exec()
