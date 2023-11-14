fb606276b0872cad6ada5565fd928dbf2bb89960
Jean-Christophe Martin <jean-christophe.martin@uha.fr>
2023-11-14 15:01:16 +0100

    commit

--- Client-Serveur/server.py
+++ Client-Serveur/server.py
@@ -1,23 +1,13 @@
-
-<<<<<<< Updated upstream
-# -*- coding: latin-1 -*-
-
-import socket # Import socket module
-
-s = socket.socket()
-nom_hote = socket.gethostname()
-addr_ip_hote = socket.gethostbyname(nom_hote)
-=======
+from threading import Thread
+import socket
 
 def Send(client):
     while True:
         msg = input()
         msg = msg.encode()
         client.send(msg)
-
-
 def Reception(client):
-    while server==1:
+    while True:
         requete_client = client.recv(1024)
         requete_client = requete_client.decode()
         print(requete_client)
@@ -27,50 +17,25 @@ def Reception(client):
 
 Host = "0.0.0.0"
 Port = 4090
-server =1
 
 #Creation du socket
-socket = socket.socket(socket.AF_INET)
+socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-
->>>>>>> Stashed changes
-
-
-<<<<<<< Updated upstream
-port = 4090
-s.bind((nom_hote, port))
-=======
-print("Serveur de test d'envoi de messages")
-print("Serveur demarre, en attente d'une connexion ...")
+socket.bind((Host,Port))
+socket.listen(1)
+print("serveur pret ... ")
 
 #Le script s'arrete jusqu'a une connection
 client, ip = socket.accept()
 print("Le client d'ip",ip,"se connecte")
->>>>>>> Stashed changes
 
-print("Serveur de test d'envoi de messages")
-print ("->nom du serveur %s"%nom_hote)
-print( "->IP de l'hote : %s"%addr_ip_hote)
-print( "->port :%i"%port)
-print("Serveur démarré, en attente d'une connexion ...")
+envoi = Thread(target=Send,args=[client])
+recep = Thread(target=Reception,args=[client])
 
+envoi.start()
+recep.start()
 
-s.listen(5)
+recep.join()
 
-
-c, addr = s.accept()
-addr_ip_client = addr[0]
-print("Connection recue depuis le client : ",addr_ip_client)
-
-message="Connecté au serveur !"
-c.send(message.encode())
-
-while True:
-    msgClient = c.recv(1024)
-    print ("C>", msgClient)
-    if msgClient.upper()== "FIN" or msgClient == "":
-        break
-        msgServeur = raw_input("S> ")
-        c.send(msgServeur)
-# on ferme la connexion
-        c.close()
+client.close()
+socket.close()
