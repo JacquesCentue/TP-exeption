21623b0aafceeeb1474213e2d6f1840f64f4a755
Jean-Christophe MARTIN <jeanchristophemartin373@gmail.com>
2023-12-30 14:12:22 +0100

    commit 30/12/2023 - Final
    - Documentation finale
    - création du readme
    - ajout de docstring manquantes

---
+++ SAE/1-Readme.txt
@@ -0,0 +1,39 @@
+Bienvenue dans le readme de l'application "zabchat" conçu dans le cadre de la SAE R302 et R309
+
+dans ce repertoire, vous trouverez 3 répertoires :
+- Client
+- Docummentation
+- Server
+
+Comme leurs noms l'indique ces 3 répertoires contiennent chacun des fichiers spécifiques a leurs roles
+ 
+====== Server ======
+ ce repertoire contient :
+- un fichier .bat utile lors de l'installation du serveur sur un systeme d'exploitation Windows
+- le fichier .py est le fichier a exécuter afin de démarer le serveur (mot de passe : root)
+- le fichier zabchat.sql qui contient la base de donnée (utilisateur de la base de données : zabchat | mot de passe : sdfghjkl)
+ 
+ 
+====== Client ======
+ 
+ ce repertoire contient :
+ - un fichier .bat utile lors de l'installation du client sur un systeme d'exploitation Windows
+ - un fichier .py qui correspond au programme a executer
+ 
+====== Documentation ======
+ 
+ ce repertoire contient toute la documentation du projet dans son intégralité : installation détaillée, description des fonctions,...
+ 
+============
+
+====== Login | mots de passe ======
+
+BDD : zabchat | sdfghjkl
+application : (test | test) ou (salut | salut) ou (Jacques | test) -> a vous de créer des comptes via le serveur.
+
+
+ les autres fichiers que vous trouverez dans ce repertoire sont des backup plus anciennes que je laisse a votre disposition
+ 
+ 
+ 
+ Je vous souhaite une bonne découverte.
--- SAE/client-bdd-gui - Copie.py
+++ SAE/client-bdd-gui - Copie.py
@@ -9,6 +9,7 @@ import threading
 import cryptocode
 import hashlib
 
+
 class AuthWindow(QDialog):
     def __init__(self):
         super().__init__()
@@ -28,22 +29,22 @@ class AuthWindow(QDialog):
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('Authentification')
 
-        self.label_username = QLabel('Nom d\'utilisateur:', self)
-        self.label_password = QLabel('Mot de passe:', self)
+        self.lblUsername = QLabel('Nom d\'utilisateur:', self)
+        self.lblPassword = QLabel('Mot de passe:', self)
 
-        self.input_username = QLineEdit(self)
-        self.input_password = QLineEdit(self)
-        self.input_password.setEchoMode(QLineEdit.Password)
+        self.tbxUsername = QLineEdit(self)
+        self.tbxPassword = QLineEdit(self)
+        self.tbxPassword.setEchoMode(QLineEdit.Password)
 
-        self.login_button = QPushButton('Se connecter', self)
-        self.login_button.clicked.connect(self.authenticate_user)
+        self.btnLogin = QPushButton('Se connecter', self)
+        self.btnLogin.clicked.connect(self.authenticate_user)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.label_username)
-        layout.addWidget(self.input_username)
-        layout.addWidget(self.label_password)
-        layout.addWidget(self.input_password)
-        layout.addWidget(self.login_button)
+        layout.addWidget(self.lblUsername)
+        layout.addWidget(self.tbxUsername)
+        layout.addWidget(self.lblPassword)
+        layout.addWidget(self.tbxPassword)
+        layout.addWidget(self.btnLogin)
 
         self.setLayout(layout)
 
@@ -53,9 +54,9 @@ class AuthWindow(QDialog):
     def authenticate_user(self):
         try:
             # Envoyer le nom d'utilisateur et le mot de passe au serveur pour l'authentification
-            username = self.input_username.text()
-            password = self.input_password.text()
-            #password= cryptocode.encrypt(password, "zabchat")
+            username = self.tbxUsername.text()
+            password = self.tbxPassword.text()
+            # password= cryptocode.encrypt(password, "zabchat")
             password = hashlib.sha256(password.encode("utf-8")).hexdigest()
             print(password)
 
@@ -71,11 +72,11 @@ class AuthWindow(QDialog):
             # Recevoir la réponse du serveur
             response = self.client_socket.recv(1024).decode('utf-8')
             response = cryptocode.decrypt(response, "uhgkO4SG5SETzgs54e/ù")
-            
-            #si le message  commance avec "AUTHORIZED" on accepte la boite de dialogue
+
+            # si le message  commance avec "AUTHORIZED" on accepte la boite de dialogue
             if response.startswith("AUTHORIZED"):
                 # Analyser la réponse pour obtenir le numéro d'utilisateur et les droits d'accès, séparé par une virgule
-                _,userid, utilisateur, droits = response.split(',')
+                _, userid, utilisateur, droits = response.split(',')
                 self.userid = userid
                 self.utilisateur = utilisateur
                 self.droits = int(droits)
@@ -83,30 +84,42 @@ class AuthWindow(QDialog):
                 # Fermer la fenêtre d'authentification et afficher la fenêtre principale
                 self.accept()
             elif response.startswith("BANNED"):
-                QMessageBox.critical(self, 'Erreur d\'authentification', 'Accès refusé. Vous avez été banni veuillez contacter un administrateur')
+                QMessageBox.critical(self, 'Erreur d\'authentification',
+                                     'Accès refusé. Vous avez été banni veuillez contacter un administrateur')
                 self.client_socket.close()
             else:
                 # Afficher un message d'erreur en cas d'authentification échouée
-                QMessageBox.critical(self, 'Erreur d\'authentification','Accès refusé. Veuillez vérifier vos informations.')
+                QMessageBox.critical(self, 'Erreur d\'authentification',
+                                     'Accès refusé. Veuillez vérifier vos informations.')
                 self.client_socket.close()
 
         except Exception as e:
             print(f"Erreur lors de l'authentification: {e}")
 
     def get_credentials(self):
+        '''
+        cette classe va retourner au programme principal en envoyant un tuple de 3 parametres
+        :return: self.userid, self.utilisateur, self.droits
+        '''
         return self.userid, self.utilisateur, self.droits
+
 
 class ChangePasswordWindow(QDialog):
     '''
     classe qui permet d'afficher une fenetre de changement de mot de passe de l'utilisateur déjà connecté
     '''
+
     def __init__(self, utilisateur, client_socket):
+        '''
+        constructeur de la classe qui permet de changer de mot de passe
+        :param utilisateur:
+        :param client_socket:
+        '''
         super().__init__()
 
         self.initUI()
         self.utilisateur = utilisateur
         self.client_socket = client_socket
-
 
     def initUI(self):
         '''
@@ -144,8 +157,6 @@ class ChangePasswordWindow(QDialog):
         new_password = self.tbxNouvMDP.text()
         confirm_password = self.tbxConfirm.text()
 
-
-
         # Vérifiez que les mots de passe correspondent
         if new_password == confirm_password:
             try:
@@ -171,15 +182,20 @@ class ChangePasswordWindow(QDialog):
 
 
 class ChatWindow(QMainWindow):
-    def __init__(self,userid, utilisateur, droits, password):
+    def __init__(self, userid, utilisateur, droits, password):
+        '''
+        constructeur de la classe ChatWindow
+        :param userid: id d'utilisateur
+        :param utilisateur: nom d'utilisateur
+        :param droits: droits d'utilisateur ce qui permet d'affiches les canaux auquel il a accès
+        :param password: mot de passe de l'utilisateur
+        '''
         super().__init__()
-
-
 
         # Configuration du client
         self.HOST = '127.0.0.1'
         self.PORT = 55555
-        self.userid=userid
+        self.userid = userid
         self.utilisateur = utilisateur
         self.droits = droits
         self.password = password
@@ -201,19 +217,24 @@ class ChatWindow(QMainWindow):
         self.createMenuBar()
 
     def createMenuBar(self):
+        """
+        cette fonction mermet d'afficher le menu option
+        :return: void
+        """
         menuBar = self.menuBar()
 
         file = menuBar.addMenu("Option")
         file.addAction(self.actChangePassword)
         file.addAction(self.actExit)
 
-
     def createActions(self):
-
+        '''
+        cette fonction permettant de faire fonctionner la menu option
+        :return: void
+        '''
         self.actChangePassword = QAction("Changer de mot de passe", self)
         self.actChangePassword.setShortcut(QKeySequence("Ctrl+P"))
         self.actChangePassword.triggered.connect(self.open_change_password_window)
-
 
         self.actExit = QAction("Exit", self)
         self.actExit.setShortcut(QKeySequence("Alt+F4"))
@@ -223,19 +244,26 @@ class ChatWindow(QMainWindow):
         self.actExit.triggered.connect(self.fermeture)
 
     def fermeture(self):
+        '''
+        cette fonction est le resultat si l'utilisateur appuie sur exit dans le menu option
+        :return: 0
+        '''
         self.client_socket.close()
         self.close()
         app.exit(0)
         sys.exit(0)
 
     def open_change_password_window(self):
-        change_password_window = ChangePasswordWindow(self.utilisateur,self.client_socket)
+        change_password_window = ChangePasswordWindow(self.utilisateur, self.client_socket)
         if change_password_window.exec() == QDialog.Accepted:
             # Vous pouvez ajouter le code ici pour envoyer le nouveau mot de passe au serveur si nécessaire
             print("Mot de passe changé avec succès!")
 
-
     def initUI(self):
+        '''
+        cette fonction permet d'afficher la fenetre
+        :return: void
+        '''
         self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle('Zabchat')
 
@@ -270,9 +298,6 @@ class ChatWindow(QMainWindow):
         self.tbxComptabiliteChat.setStyleSheet("color: orange;")
         self.tbxComptabiliteChat.hide()
 
-
-
-
         self.input_line = QLineEdit(self)
         # Envoi du message si l'utilisateur appuie sur la touche entrer
         self.input_line.returnPressed.connect(self.send_message)
@@ -280,7 +305,7 @@ class ChatWindow(QMainWindow):
         self.send_button = QPushButton('Envoyer', self)
         self.send_button.clicked.connect(self.send_message)
 
-        #definission de la combobox des diférents cannaux
+        # definission de la combobox des diférents cannaux
         self.cbxCannaux = QComboBox(self)
 
         self.selectChat("General")
@@ -289,16 +314,14 @@ class ChatWindow(QMainWindow):
         self.cbxCannaux.addItem("Blabla")
         if self.droits >= 5:
             self.cbxCannaux.addItem("Informatique")
-        if self.droits ==3 or self.droits==4 or self.droits==7 or self.droits==8 :
+        if self.droits == 3 or self.droits == 4 or self.droits == 7 or self.droits == 8:
             self.cbxCannaux.addItem("Marketing")
         if self.droits == 2 or self.droits == 4 or self.droits == 6 or self.droits == 8:
             self.cbxCannaux.addItem("Comptabilite")
 
-
-
         self.cbxCannaux.currentTextChanged.connect(self.selectChat)
 
-        #affichage des widgets
+        # affichage des widgets
         layout = QVBoxLayout()
         layout.addWidget(self.lblChat)
         layout.addWidget(self.cbxCannaux)
@@ -310,13 +333,17 @@ class ChatWindow(QMainWindow):
         layout.addWidget(self.input_line)
         layout.addWidget(self.send_button)
 
-
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
-    def selectChat(self, value,):
-        #print("combobox changed", value)
+    def selectChat(self, value, ):
+        '''
+        cette fonction permet de determiner le chat selectionné de l'utilisateur
+        :param value: valeur de la chaine de caracteres selectionné dans le combobox
+        :return: void
+        '''
+        # print("combobox changed", value)
         self.lblChat.setText(value)
         if self.lblChat.text() == "General":
             self.tbxGeneralChat.show()
@@ -352,8 +379,8 @@ class ChatWindow(QMainWindow):
 
         elif self.lblChat.text() == "Comptabilite":
             try:
-                #code de detection de droit -> ceci est le code que j'avais mis en place avant d'avoir mis en place la detection des droits a l'ouverture de la fenetre
-                if self.droits== 2 or self.droits==4 or self.droits==6 or self.droits==8:
+                # code de detection de droit -> ceci est le code que j'avais mis en place avant d'avoir mis en place la detection des droits a l'ouverture de la fenetre
+                if self.droits == 2 or self.droits == 4 or self.droits == 6 or self.droits == 8:
                     self.tbxGeneralChat.hide()
                     self.tbxBlablaChat.hide()
                     self.tbxInformatiqueChat.hide()
@@ -367,34 +394,38 @@ class ChatWindow(QMainWindow):
                                      'Accès refusé. Vous n\'avez pas les droits de consulter ce canal.')
                 self.cbxCannaux.removeItem("Comptabilite")
 
-
-
-
     def receive_messages(self):
-        client_status=1
-        while client_status==1:
+        '''
+        fonction qui permet de recevoir les messages les messages seront analysés afin de determiner dans quel canal le message appartient
+        :return:
+        '''
+        client_status = 1
+        while client_status == 1:
 
             try:
-                #print(self.droits)
+                # print(self.droits)
                 # Réception du message du serveur
                 message = self.client_socket.recv(1024).decode('utf-8')
-                #on décrypte le message avec le mot de passe
+                # on décrypte le message avec un mot de passe
                 message = cryptocode.decrypt(message, "uhgkO4SG5SETzgs54e/ù")
-               
-                #on determine a quel cannal le message apartient
+
+                # on determine a quel cannal le message apartient
                 if message.startswith("/Blabla"):
                     messagetransmi = message.split()
                     messagetransmi = ' '.join(messagetransmi[1:])
                     self.tbxBlablaChat.append(messagetransmi)
-                elif message.startswith("/Informatique") and (self.droits ==5 or self.droits==6 or self.droits==7 or self.droits==8):
+                elif message.startswith("/Informatique") and (
+                        self.droits == 5 or self.droits == 6 or self.droits == 7 or self.droits == 8):
                     messagetransmi = message.split()
                     messagetransmi = ' '.join(messagetransmi[1:])
                     self.tbxInformatiqueChat.append(messagetransmi)
-                elif message.startswith("/Marketing") and (self.droits ==3 or self.droits==4 or self.droits==7 or self.droits==8):
+                elif message.startswith("/Marketing") and (
+                        self.droits == 3 or self.droits == 4 or self.droits == 7 or self.droits == 8):
                     messagetransmi = message.split()
                     messagetransmi = ' '.join(messagetransmi[1:])
                     self.tbxMarketingChat.append(messagetransmi)
-                elif message.startswith("/Comptabilite") and (self.droits== 2 or self.droits==4 or self.droits==6 or self.droits==8):
+                elif message.startswith("/Comptabilite") and (
+                        self.droits == 2 or self.droits == 4 or self.droits == 6 or self.droits == 8):
                     messagetransmi = message.split()
                     messagetransmi = ' '.join(messagetransmi[1:])
                     self.tbxComptabiliteChat.append(messagetransmi)
@@ -405,7 +436,7 @@ class ChatWindow(QMainWindow):
                 elif message.startswith("AUTHORIZED") or message.startswith("/ChangePassword"):
 
                     print(message)
-                elif message=="QUIT":
+                elif message == "QUIT":
                     self.client_socket.close()
                     app.exit(0)
                     sys.exit(0)
@@ -418,33 +449,34 @@ class ChatWindow(QMainWindow):
                 # En cas d'erreur, fermer la connexion du client
                 print(f"Erreur lors de la réception du message. {e}")
                 self.client_socket.close()
-                break
+                client_status = 0
 
     def send_message(self):
+        '''
+        fonction qui permet d'envoyer les messages au serveur
+        :return: void
+        '''
         # Envoi du message au serveur
         message = self.input_line.text().strip()
         # verification du contenu du message (si il n'est pas vide)
         if message != "":
-            if message !="bye":
-
+            if message != "bye":
 
                 message = (f"/{self.lblChat.text()} {self.utilisateur}> {self.input_line.text()}")
-                #on chiffre le message afin qu'il soi cachés aux clients frauduleux
+                # on chiffre le message afin qu'il soi cachés aux clients frauduleux
                 message = cryptocode.encrypt(message, "earg45rsy72zerg")
-                #print(message)
+                # print(message)
                 self.client_socket.send(message.encode('utf-8'))
-                #self.tbxGeneralChat.append(f"vous> {message}")
-
+                # self.tbxGeneralChat.append(f"vous> {message}")
 
                 self.input_line.clear()
             else:
                 message = ("bye")
-
+                message = cryptocode.encrypt(message, "earg45rsy72zerg")
                 self.client_socket.send(message.encode('utf-8'))
                 time.sleep(0.5)
                 self.client_socket.close()
                 app.quit()
-
 
 
 if __name__ == '__main__':
@@ -452,7 +484,7 @@ if __name__ == '__main__':
     auth_window = AuthWindow()
     if auth_window.exec() == QDialog.Accepted:
         userid, utilisateur, droits = auth_window.get_credentials()
-        client_window = ChatWindow(userid,utilisateur, droits, auth_window.input_password.text())
+        client_window = ChatWindow(userid, utilisateur, droits, auth_window.tbxPassword.text())
         client_window.show()
         sys.exit(app.exec_())
         sys.exit(0)
